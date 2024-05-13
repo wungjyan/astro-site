17af0812a1d1871bc8a99c8ef06b45c07b7e2c93
wungjyan <1170017412@qq.com>
2024-05-13 10:32:34 +0800

    refactor: remove startViewTransition mode

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -12,11 +12,6 @@ let themeValue = getPreferTheme();
 function setPreference() {
   localStorage.setItem("theme", themeValue);
   document.firstElementChild.setAttribute("data-theme", themeValue);
-  if (themeValue === "dark") {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
 }
 
 setPreference();
@@ -25,56 +20,13 @@ window.onload = () => {
   function setThemeFeature() {
     setPreference();
     document.querySelector("#theme-btn")?.addEventListener("click", (event) => {
-      const isSupport =
-        document.startViewTransition &&
-        !window.matchMedia("(prefers-reduced-motion: reduce)").matches;
-      if (!isSupport) {
-        toggleDark();
-        return;
-      }
-      toggleViewTransition(event);
+      themeValue = themeValue === "light" ? "dark" : "light";
+      setPreference();
     });
   }
   setThemeFeature();
   document.addEventListener("astro:after-swap", setThemeFeature);
 };
-
-function toggleDark() {
-  themeValue = themeValue === "light" ? "dark" : "light";
-  setPreference();
-}
-
-function toggleViewTransition(event) {
-  const x = event.clientX;
-  const y = event.clientY;
-  const endRadius = Math.hypot(
-    Math.max(x, innerWidth - x),
-    Math.max(y, innerHeight - y)
-  );
-  const clipPath = [
-    `circle(0px at ${x}px ${y}px)`,
-    `circle(${endRadius}px at ${x}px ${y}px)`,
-  ];
-  const transition = document.startViewTransition(() => {
-    toggleDark();
-  });
-
-  transition.ready.then(() => {
-    document.documentElement.animate(
-      {
-        clipPath: themeValue === "light" ? [...clipPath].reverse() : clipPath,
-      },
-      {
-        duration: 500,
-        easing: "ease-in",
-        pseudoElement:
-          themeValue === "light"
-            ? "::view-transition-old(root)"
-            : "::view-transition-new(root)",
-      }
-    );
-  });
-}
 
 window
   .matchMedia("(prefers-color-scheme: dark)")
